4cbf8253ff0e12bed4e250da2bad6da350f743fa
Greg Leppert <greg@formasfunction.com>
2011-11-06 09:49:32 -0500

    Adding package files

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "signature",
+  "description": "Simple key/secret based authentication for apis, ported from the Ruby gem by the same name",
+  "version": "0.0.1",
+  "homepage": "https://github.com/leppert/signature",
+  "author": "Greg Leppert <greg@formasfunction.com>",
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/leppert/signature.git"
+  },
+  "main": "lib/signature",
+  "engines": {
+    "node": "~ 0.4.8"
+  }
+}
+
